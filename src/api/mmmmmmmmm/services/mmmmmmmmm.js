17fd79c0e77a4b55fb54839c56866f1f6559a357
Mohamedabdullhaleam <mohamedlimo236@gmail.com>
2024-03-20 07:59:18 +0200

    Creating content type from external DB schema && error handling v-1

--- src/api/mmmmmmmmm/services/mmmmmmmmm.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * mmmmmmmmm service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::mmmmmmmmm.mmmmmmmmm');
